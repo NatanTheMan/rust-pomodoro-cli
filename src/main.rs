eb6deee99f06a680cdaf58429b2dfc6cf8a1180a
Natan <natanscosta7@gmail.com>
2025-09-27 10:27:43 -0300

    init dunst if it was down

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,37 @@ use std::process::Command;
 use std::{thread, time::Duration};
 
 fn main() {
+    start_notification_daemon();
     loop {
         let pomodoro: u16 = 15; // 25min
         thread::sleep(Duration::from_secs(pomodoro as u64));
-        print_pomodoro_finished();
-
+        spawn_notification("Tempo de fazer uma pausa");
         let rest: u16 = 3; // 5min
         thread::sleep(Duration::from_secs(rest as u64));
-        print_rest_finished();
+        spawn_notification("Volte ao Trabalho");
     }
 }
 
-fn print_rest_finished() {
+fn start_notification_daemon() {
+    let status = Command::new("pgrep").arg("dunst").status();
+    println!("\x1B[2J\x1B[1;1H");
+    match status {
+        Ok(s) if s.code() == Some(1) => run_dunst(),
+        Ok(_) => (),
+        Err(e) => println!("{}", e),
+    }
+}
+
+fn run_dunst() {
+    Command::new("dunst").spawn().expect("cant run Dunst");
+}
+
+fn spawn_notification(message: &str) {
     Command::new("notify-send")
         .arg("-u")
         .arg("critical")
         .arg("Pomodoro")
-        .arg("Volte ao Trabalho")
+        .arg(message)
         .spawn()
-        .expect("error");
-}
-
-fn print_pomodoro_finished() {
-    Command::new("notify-send")
-        .arg("-u")
-        .arg("normal")
-        .arg("Pomodoro")
-        .arg("Tempo de fazer uma pausa")
-        .spawn()
-        .expect("error");
+        .expect("error while sending notification");
 }
