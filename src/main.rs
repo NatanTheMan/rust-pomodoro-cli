0a2511d1132761d77b6015d6d07790b717cc1657
Natan <natanscosta7@gmail.com>
2025-09-27 17:04:11 -0300

    accept flags for pomodoro time, short and long interval

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,69 @@
 use std::env::args;
-use std::num::ParseIntError;
 use std::process::Command;
 use std::{thread, time::Duration};
 
 fn main() {
-    start_notification_daemon();
+    // IN MINUTES
+    let mut pomodoro_time: u16 = 25;
+    let mut short_interval_time: u16 = 5;
+    let mut long_interval_time: u16 = 15;
 
     let mut args = args().skip(1);
-
     while let Some(arg) = args.next() {
         match arg.as_str() {
-            "-p" => {
-                if let Err(_) = args
+            "-p" | "--pomodoro" => {
+                match args
                     .next()
-                    .expect("error while reading arg after flag")
-                    .parse::<i32>()
+                    .expect("error while reading pomodoro time")
+                    .parse::<u16>()
                 {
-                    println!("invalid arg, using default values.");
-                } else {
-                    println!("tudo certo");
+                    Ok(time) => {
+                        pomodoro_time = time;
+                        println!("pomodoro {}", time)
+                    }
+                    Err(_) => println!("invalid arg, using default value for Pomodoro."),
                 }
             }
-            _ => (),
+            "-s" | "--short-interval" => {
+                match args
+                    .next()
+                    .expect("error while reading short time")
+                    .parse::<u16>()
+                {
+                    Ok(time) => {
+                        short_interval_time = time;
+                        println!("short {}", time)
+                    }
+                    Err(_) => println!("invalid arg, using default value for Pomodoro."),
+                }
+            }
+            "-l" | "--long-interval" => {
+                match args
+                    .next()
+                    .expect("error while reading long time")
+                    .parse::<u16>()
+                {
+                    Ok(time) => {
+                        long_interval_time = time;
+                        println!("long {}", time)
+                    }
+                    Err(_) => println!("invalid arg, using default value for Pomodoro."),
+                }
+            }
+            _ => println!("sem args"),
         }
     }
 
-    loop {
-        let pomodoro: u16 = 15; // 25min
-        thread::sleep(Duration::from_secs(pomodoro as u64));
-        spawn_notification("Tempo de fazer uma pausa");
-        let rest: u16 = 3; // 5min
-        thread::sleep(Duration::from_secs(rest as u64));
-        spawn_notification("Volte ao Trabalho");
-    }
+    start_notification_daemon();
+
+    run_pomodoro(pomodoro_time, short_interval_time, long_interval_time);
+}
+
+fn run_pomodoro(pomodoro_time: u16, short_interval_time: u16, long_interval_time: u16) {
+    thread::sleep(Duration::from_secs(pomodoro_time as u64) / 100);
+    spawn_notification("Tempo de fazer uma pausa");
+    thread::sleep(Duration::from_secs(short_interval_time as u64) / 100);
+    spawn_notification("Volte ao Trabalho");
 }
 
 fn start_notification_daemon() {
