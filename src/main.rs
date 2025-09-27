b9ea4285e317baccbb528cf4eac9979cb2f20c14
Natan <natanscosta7@gmail.com>
2025-09-27 07:03:52 -0300

    first functional version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::process::Command;
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let pomodoro: u16 = 15; // 25min
+        thread::sleep(Duration::from_secs(pomodoro as u64));
+        print_pomodoro_finished();
+
+        let rest: u16 = 3; // 5min
+        thread::sleep(Duration::from_secs(rest as u64));
+        print_rest_finished();
+    }
+}
+
+fn print_rest_finished() {
+    Command::new("notify-send")
+        .arg("-u")
+        .arg("critical")
+        .arg("Pomodoro")
+        .arg("Volte ao Trabalho")
+        .spawn()
+        .expect("error");
+}
+
+fn print_pomodoro_finished() {
+    Command::new("notify-send")
+        .arg("-u")
+        .arg("normal")
+        .arg("Pomodoro")
+        .arg("Tempo de fazer uma pausa")
+        .spawn()
+        .expect("error");
 }
